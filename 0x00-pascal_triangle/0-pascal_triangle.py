580113e106a06caf24da14394b449a4fea0a317b
uelkariuki <cratzuel@gmail.com>
2023-11-30 03:11:48 +0300

    Fix Style errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,7 +4,10 @@
 
 
 def pascal_triangle(n):
-    """ Returns a list of lists of integers representing the Pascals triangle of n"""
+    """
+	Returns a list of lists of integers representing the
+	Pascals triangle of n
+	"""
     if n <= 0:
         return []
 
@@ -14,7 +17,7 @@ def pascal_triangle(n):
         temp = [0] + result[-1] + [0]
         row = []
 
-        for j in range(len(result[-1])+ 1):
+        for j in range(len(result[-1]) + 1):
             row.append(temp[j] + temp[j + 1])
         result.append(row)
     return result
