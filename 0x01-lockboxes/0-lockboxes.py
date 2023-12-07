e014287e703d30d80015a27efc23cb3d54f395ec
uelkariuki <cratzuel@gmail.com>
2023-12-07 16:32:20 +0300

    Fix Pycodestyle errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,19 +4,17 @@ You have n number of locked boxes in front of you. Each box is numbered
 sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 """
 
+
 def canUnlockAll(boxes):
-	""" method that determines if all the boxes can be opened"""
-	unlocked_boxes = [False] * len(boxes)
-	unlocked_boxes[0] = True
-	stack = set(boxes[0])
-	while stack:
-		key = stack.pop()
+    """ method that determines if all the boxes can be opened"""
+    unlocked_boxes = [False] * len(boxes)
+    unlocked_boxes[0] = True
+    stack = set(boxes[0])
+    while stack:
+        key = stack.pop()
 
-		if key < len(boxes) and not unlocked_boxes[key]:
-			unlocked_boxes[key] = True
-			stack.update(boxes[key])
+        if key < len(boxes) and not unlocked_boxes[key]:
+            unlocked_boxes[key] = True
+            stack.update(boxes[key])
 
-	return all(unlocked_boxes)
-
-
-
+    return all(unlocked_boxes)
