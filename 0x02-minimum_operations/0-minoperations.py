2f9e0729c71b707375a88c50087e2c7cc254377c
uelkariuki <cratzuel@gmail.com>
2023-12-14 18:57:28 +0300

    Fix method prototype

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -19,15 +19,15 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-    divisor: int = 2
-    min_no_Operations: int = 0
+    divisor = 2
+    operations = 0
 
     while n > 1:
         if n % divisor == 0:
             n = n / divisor
-            min_no_Operations = min_no_Operations + divisor
+            operations = operations + divisor
 
         else:
             divisor += 1
 
-    return min_no_Operations
+    return operations
