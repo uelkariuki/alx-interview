c455887696ce4d183e39db2baeef7685bfb2eb00
uelkariuki <cratzuel@gmail.com>
2024-01-05 01:22:55 +0300

    Implement UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,28 +10,27 @@ def validUTF8(data: List[int]) -> bool:
     Method that determines if a given data set represents
     a valid UTF-8 encoding
     """
-    no_of_bytes = 0
 
-    for value in data:
-        if not 0 <= value <= 255:
-            return False
-        if no_of_bytes == 0:
-            # check first 3 bits if 110, its 2-byte character
-            if (value >> 5) == 0b110:
-                no_of_bytes = 1
-            elif (value >> 4) == 0b1110:
-                # expecting 2 more bytes for this character
-                no_of_bytes = 2
-            elif (value >> 3) == 0b11110:
-                no_of_bytes = 3
-            elif (value >> 7) == 0:
-                continue
+    def countOne(num):
+        """Method to count number of ones"""
+        count = 0
+        for i in range(7, -1, -1):
+            if num & (1 << i):
+                count += 1
             else:
+                break
+        return count
+    count = 0
+    for x in data:
+        if not count:
+            count = countOne(x)
+            if count == 0:
+                continue
+            if count == 1 or count > 4:
                 return False
+            count -= 1
         else:
-            if (value >> 6) != 0b10:
+            count -= 1
+            if countOne(x) != 1:
                 return False
-            # subtract as we processed one of the
-            # continuation bytes
-            no_of_bytes -= 1
-    return no_of_bytes == 0
+    return count == 0
